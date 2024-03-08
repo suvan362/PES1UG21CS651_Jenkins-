b62b11b9218c4490240f12d28a9f2287210f949a
suvan362 <40496225+suvan362@users.noreply.github.com>
2024-03-08 22:51:15 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ int main() {
     int a = 100;
     int b = 200;
     int sum = a + b;
-    std::cout << "The sum of " << a << " + " << b << " is: " << sum << std::endl;
+    std::cout << "The sum of " << a << " + " << b << " is: " << sum << std::endl
     return 0;
 }
