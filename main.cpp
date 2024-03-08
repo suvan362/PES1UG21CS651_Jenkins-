e4d61b4d60c2bcfda9bab3b2ab02a76c7cbc2c6a
suvan362 <40496225+suvan362@users.noreply.github.com>
2024-03-08 22:46:05 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int a = 100;
+    int b = 200;
+    int sum = a + b;
+    std::cout << "The sum of " << a << " + " << b << " is: " << sum << std::endl;
+    return 0;
+}
